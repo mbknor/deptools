3abd3076f2196dbfaadc74298011757f866daf7c
Morten Kjetland <mbk@kjetland.com>
2010-03-24 19:09:23 +0100

    added test cases for include-exclude code

--- maven-deptools-plugin-scala/src/test/scala/deptools/plugin/scala/parser/filter/DependencyMatcherTest.scala
+++ maven-deptools-plugin-scala/src/test/scala/deptools/plugin/scala/parser/filter/DependencyMatcherTest.scala
@@ -64,5 +64,72 @@ class DependencyFilterIncludeExcludeImplTest{
       new Dependency("g1", "a2", "jar", "v", "compile"),
       new Dependency("g2", "a11", "jar", "v", "compile")).toArray
     Assert.assertEquals( false, dm.matches(depPath))
+
+    dm = new DependencyFilterIncludeExcludeImpl(null, null)
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( true, dm.matches(depPath))
+
+    dm = new DependencyFilterIncludeExcludeImpl("xx", null)
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( false, dm.matches(depPath))
+
+    dm = new DependencyFilterIncludeExcludeImpl("g2.*", null)
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( true, dm.matches(depPath))
+
+    dm = new DependencyFilterIncludeExcludeImpl("g1:a2", null)
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( true, dm.matches(depPath))
+
+    dm = new DependencyFilterIncludeExcludeImpl(null, "g2.*")
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( false, dm.matches(depPath))
+
+    dm = new DependencyFilterIncludeExcludeImpl(null, "g1:a2")
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( false, dm.matches(depPath))
+
+    //this should go through since g1:a1 is the first dependency
+    dm = new DependencyFilterIncludeExcludeImpl(null, "g1:a1")
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( true, dm.matches(depPath))
+
+    //include match and exclude does not match -> ok
+    dm = new DependencyFilterIncludeExcludeImpl("g.*", "xx")
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( true, dm.matches(depPath))
+
+
+    //include matches but exclude excludes it -> no match
+    dm = new DependencyFilterIncludeExcludeImpl("g1:.*", "g1:a2")
+    depPath = List(
+      new Dependency("g1", "a1", "jar", "v", "compile"),
+      new Dependency("g1", "a2", "jar", "v", "compile"),
+      new Dependency("g2", "a11", "jar", "v", "compile")).toArray
+    Assert.assertEquals( false, dm.matches(depPath))
   }
 }
